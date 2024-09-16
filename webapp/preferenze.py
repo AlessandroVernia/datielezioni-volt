0c6bf8b59d377e2a89c6e892d4e785f409972597
Alessandro Vernia <alessandrovernia@gmail.com>
2024-09-16 11:05:35 +0200

    moved maps in dedicated directory under templates; updated gitignore

--- webapp/preferenze.py
+++ webapp/preferenze.py
@@ -8,39 +8,39 @@ def index():
 
 @app.route('/ele_t_10000_totale')
 def ele_t_10000_totale():
-    return render_template('ele_t_10000_totale.html')
+    return render_template('maps/ele_t_10000_totale.html')
 
 @app.route('/vot_t_10000_totale')
 def vot_t_10000_totale():
-    return render_template('vot_t_10000_totale.html')
+    return render_template('maps/vot_t_10000_totale.html')
 
 @app.route('/voti_pd_10000_totale')
 def voti_pd_10000_totale():
-    return render_template('voti_pd_10000_totale.html')
+    return render_template('maps/voti_pd_10000_totale.html')
 
 @app.route('/ele_t_10000_silvia')
 def ele_t_10000_silvia():
-    return render_template('ele_t_10000_silvia.html')
+    return render_template('maps/ele_t_10000_silvia.html')
 
 @app.route('/vot_t_10000_silvia')
 def vot_t_10000_silvia():
-    return render_template('vot_t_10000_silvia.html')
+    return render_template('maps/vot_t_10000_silvia.html')
 
 @app.route('/voti_pd_10000_silvia')
 def voti_pd_10000_silvia():
-    return render_template('voti_pd_10000_silvia.html')
+    return render_template('maps/voti_pd_10000_silvia.html')
 
 @app.route('/ele_t_10000_marcello')
 def ele_t_10000_marcello():
-    return render_template('ele_t_10000_marcello.html')
+    return render_template('maps/ele_t_10000_marcello.html')
 
 @app.route('/vot_t_10000_marcello')
 def vot_t_10000_marcello():
-    return render_template('vot_t_10000_marcello.html')
+    return render_template('maps/vot_t_10000_marcello.html')
 
 @app.route('/voti_pd_10000_marcello')
 def voti_pd_10000_marcello():
-    return render_template('voti_pd_10000_marcello.html')
+    return render_template('maps/voti_pd_10000_marcello.html')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8000, debug=True)
